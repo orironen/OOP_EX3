39fe9eb2d517d3dd95d4884e73161265a2a2098d
e10withadot <e10withadot@gmail.com>
2025-01-07 14:36:36 +0200

    added logging

--- database/library.py
+++ database/library.py
@@ -2,7 +2,15 @@ from copy import deepcopy
 import csv
 from book import Book, Genre
 
-def yesNoBool(string: str) -> bool:
+def csvAsMatrix(file: str) -> list:
+    """
+    Returns a matrix representing the rows of the csv file.
+    """
+    with open(file, 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        return list(reader)
+
+def __yesNoBool(string: str) -> bool:
     """
     Returns True if the string is 'Yes', False otherwise.
     """
@@ -10,7 +18,7 @@ def yesNoBool(string: str) -> bool:
         return True
     return False
 
-def boolYesNo(boolean: bool) -> str:
+def __boolYesNo(boolean: bool) -> str:
     """
     Returns 'Yes' if the boolean is True, 'No' otherwise.
     """
@@ -23,6 +31,7 @@ class Library:
     Represents a library database. Contains functions to manage books and users.
     """
     def __init__(self, users):
+        # get books from csv
         with open('books.csv', 'r', newline='') as books:
             booklist = csv.reader(books, delimiter=',')
             self.books = []
@@ -30,64 +39,79 @@ class Library:
                 self.books.append(Book(
                     row[0], 
                     row[1], 
-                    yesNoBool(row[2]), 
+                    __yesNoBool(row[2]), 
                     int(row[3]), 
                     Genre.parseGenre(row[4]), 
                     int(row[5])
                 ))
         self.users= deepcopy(users)
+        self.log= []
+        with open('log.txt', 'w') as log:
+            log.write('')
+
+    def __log__(self, action: str):
+        """
+        Logs an action into the log file.
+        """
+        with open('log.txt', 'a') as log:
+            log.write(f'{action}\n')
 
     def addBook(self, book: Book):
         """
         Adds a book to the library.
         """
         self.books.append(book)
-        with open('books.csv', 'a', newline='') as books:
-            booklist = csv.writer(books, delimiter=',')
-            booklist.writerow([
-                book._title,
-                book._author,
-                yesNoBool(book.wasLoaned()),
-                book._copies,
-                str(book._genre),
-                book._year
-            ])
+        try:
+            # add new book to csv
+            with open('books.csv', 'a', newline='') as books:
+                booklist = csv.writer(books, delimiter=',')
+                booklist.writerow([
+                    book._title,
+                    book._author,
+                    __yesNoBool(book.wasLoaned()),
+                    book._copies,
+                    str(book._genre),
+                    book._year
+                ])
+            self.__log__('book added successfully')
+        except:
+            self.__log__('book added fail')
 
     def removeBook(self, book: Book):
         """
         Removes a book from the library.
         """
         self.books.remove(book)
-        # read csv
-        with open('books.csv', 'r', newline='') as books:
-            bookreader = csv.reader(books, delimiter=',')
-            bookfile = list(bookreader)
-        # remove relevant row
-        for row in bookfile:
-            if row[0] == book._title:
-                bookreader.remove(row)
-        # rejoin csv
-        output= ','.join(row for row in bookfile)
-        with open('books.csv', 'w', newline='') as books:
-            books.write(output)
-
+        try:
+            # read csv
+            bookfile= csvAsMatrix('books.csv')
+            # remove relevant row
+            for row in bookfile:
+                if row[0] == book._title:
+                    bookfile.remove(row)
+            # rejoin file
+            output= ','.join(row for row in bookfile)
+            # write to csv
+            with open('books.csv', 'w', newline='') as books:
+                books.write(output)
+            self.__log__('book removed successfully')
+        except:
+            self.__log__('book removed fail')
 
     def updateBookDetails(self, index: int, newBook: Book):
         """
-        Updates the details of a book.
+        Updates the details of the book in the specied index.
         """
         self.books[index]= newBook
         # read csv
-        with open('books.csv', 'r', newline='') as books:
-            bookreader = csv.reader(books, delimiter=',')
-            bookfile = list(bookreader)
+        bookfile= csvAsMatrix('books.csv')
         # update relevant row
         for i in len(bookfile):
             if i == index:
                 bookfile[index]= [
                     newBook._title,
                     newBook._author,
-                    boolYesNo(newBook.wasLoaned()),
+                    __boolYesNo(newBook.wasLoaned()),
                     newBook._copies,
                     str(newBook._genre),
                     newBook._year
