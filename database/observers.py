4e088285a0ad556e50ba628204f740e955d66840
e10withadot <e10withadot@gmail.com>
2025-01-08 17:17:13 +0200

    small insiginificant changes lol

---
+++ database/observers.py
@@ -0,0 +1,12 @@
+class __Observer:
+    """
+    Implementation of the Observer design pattern.
+    """
+    def __init__(self, observers: list):
+        self.observers= observers
+
+    def addObservers(self, newObservers: list):
+        pass
+
+    def notify():
+        pass
